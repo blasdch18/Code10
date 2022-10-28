2aae37a28fa4a37570740f570f4bd656d5bf0559
Blas Cruz <blas.cruz@ucsp.edu.pe>
2022-10-27 20:56:32 -0500

    image homework

--- react-basics/github-project/src/App.js
+++ react-basics/github-project/src/App.js
@@ -1,108 +1,56 @@
-//import logo from './logo.svg';
-import './App.css';
-import Header from "./components/Header";
-
-import sun from "./assets/icons/sun-icon-15.png";
-import git from "./assets/icons/git.png";
-import link from "./assets/icons/link.png";
-import perfil from "./assets/icons/perfil.png";
+import { useState } from "react";
+import Userinformation from "./components/UserInformation";
+import "./App.css";
 import search from "./assets/icons/search.png";
-import twitter from "./assets/icons/twitter.png";
-import ubi from "./assets/icons/ubi.png";
-import perfil_ from "./assets/icons/perfil.jpg";
+import sun from "./assets/icons/sun.png";
 
 function App() {
-  function saludar() {
-    console.log("Hola Mundo");
+  const [inputText, setInputText] = useState("");
+
+  const [user, setUser] = useState(null);
+
+  function handleInputChange(event) {
+    setInputText(event.target.value);
   }
+
+  async function searchUser() {
+    try {
+      const response = await fetch(`https://api.github.com/users/${inputText}`);
+      const data = await response.json();
+      setUser(data);
+    } catch (error) {
+      console.log("Error", error);
+    }
+  }
+
   return (
     <div className="container">
-
       <div className="section-1">
-          <h4>devfinder</h4>
-          <button className="btn-mode" >
-            LIGHT <img width="18" src={sun} />
-          </button>
+        <h4 className="title">devfinder</h4>
+        <button className="btn-mode">
+          LIGHT <img width="20" src={sun} alt="" />
+        </button>
       </div>
-
-      <div className="section-2">
-        <div className="input-section-2">
-          <img width="30" src={search} />
-          <input 
-            className="input-search-section-2"
-            type="text" 
+      <div className="search-container">
+        <div className="input-container">
+          <img width="15" src={search} alt="" />
+          <input
+            value={inputText}
+            onChange={handleInputChange}
+            className="input-search"
+            type="text"
             placeholder="Search GitHub username..."
           />
         </div>
         <div>
-          <button className="btn-search">Search</button>
+          <button className="btn-search" onClick={searchUser}>
+            Search
+          </button>
         </div>
       </div>
-      
-      <div>
-          
-      </div>
-    
-      <div className="profile-content">
-      
-          <div className="image-profile-content">
-            <img width="100" src={perfil_} alt="" />
-          </div>
-      
-            
-          <div className="info-profile">
-          
-            <div className="title-profile">
-              <h4>The Octocat</h4>
-              <p>Joined 25 Jan 2011</p>
-            </div>
-      
-            <div className="subsection-profile">
-              <p className="arroba">@octocat</p>
-              <p>This profile has no bio</p>
-            </div>
-          
-            <div className="details-profile">
-              <div>
-                <h6>Repos</h6>
-                <h4>8</h4>
-              </div>
-              <div>
-                <h6>Followers</h6>
-                <h4>3938</h4>
-              </div>
-              <div>
-                <h6>Following</h6>
-                <h4>9</h4>
-              </div>
-            </div>
-           
-            <div className="footer-profile">
-              <div>
-                <p>
-                  <img width="100" src={ubi} alt="" />
-                  San Francisco
-                </p>
-                <p className="httpss">
-                  <img width="100" src={link} alt="" />                   
-                  https://github.blog
-                </p>
-              </div>
-              <div>
-                <p className="twitter-letter">
-                  <img width="100" src={twitter} alt="" />
-                  Not available
-                </p>
-                <p>
-                  <img width="100" src={git} alt="" />
-                  @blasdch18
-                </p>
-              </div>
-            </div>
-          </div>
-</div>
-</div>
- 
+      {/* aca podemos hacer una validacion donde digamos que el div que sigue exista siempre y cuando user tengo datos */}
+      {user && <Userinformation user={user} />}
+    </div>
   );
 }
 
--- react-basics/github-project/src/components/Header.js
+++ react-basics/github-project/src/components/Header.js
@@ -1,16 +1,14 @@
-function Header () {
-    return (
-        <div>
-            <h1> Este es mi Componente Header</h1>
-            <ul>
-                <li>Inicio</li>
-                <li>
-                    Nosotros
-                </li>
-                <li>Proyectos</li>
-            </ul>
-        </div>
-    );
+function Header() {
+  return (
+    <div>
+      <h2>Este es mi header</h2>
+      <ul>
+        <li>Inicio</li>
+        <li>Nosotros</li>
+        <li>Proyectos</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Header;
