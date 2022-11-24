bd5eeac7142946b36986ae0f67ea8e61ea0dcbae
Blas Cruz <blas.cruz@ucsp.edu.pe>
2022-11-23 19:50:51 -0500

    useRef()

--- react-basics/proj2/src/components/TaskForm/index.jsx
+++ react-basics/proj2/src/components/TaskForm/index.jsx
@@ -1,16 +1,18 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 function TaskForm () {
     const [inputTask, setInputTask] = useState("");
+    const inputElement = useRef(null);
 
 
     function taskInput(e) {
-        inputTask = e.target.value; // Nadie va a reaccionar
+        //inputTask = e.target.value; // Nadie va a reaccionar, por defecto entra
         setInputTask(e.target.value); // Todos van a reaccionar
     }
 
     function addButton () {
-        console.log("hola");
+        console.log(inputElement.current.value);
+        inputElement.current.value = "";
     }
 
     return (
@@ -21,6 +23,8 @@ function TaskForm () {
                         type="text"
                         className="form-control"
                         placeholder="My new task ..."
+                        onChange={taskInput}
+                        ref={inputElement}
                     />
                     <button 
                         className="btn btn-outline-primary" 
