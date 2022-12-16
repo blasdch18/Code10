9a7dca0321a6ba5f2ac41204f49772cd4787743a
Blas Cruz <blas.cruz@ucsp.edu.pe>
2022-12-16 00:18:12 -0500

    status

--- react-basics/proj2/src/components/TaskCard/index.jsx
+++ react-basics/proj2/src/components/TaskCard/index.jsx
@@ -2,34 +2,47 @@ function TaskCard (props) {
 
 	const { task, updateTask  } = props;
 
+	const statusClass = {
+		1:"bg-primary",
+		2:"bg-success",
+		3:"bg-danger",
+	};
+
     return (
-        <div className="mt-3 card p-3 mt-3 shadow-sm">
+        <div 
+			className={`mt-3 card p-3 mt-3 shadow-sm bg-opacity-25 ${
+				statusClass[task.status]
+	        }`}
+		>
 			<div className="d-flex">
-				<span className="me-3">
-					<>
-					</>
-					<button
-						onClick={()=> updateTask(task.id)}	
-						className="btn btn-sm btn-outline-primary py-0 small opacity-50">
-						✓
-					</button>
-				</span>	
-				<span>{task.name}</span>		
+				{task.status === 1 && (
+					<span className="me-3">
+						<button					
+							onClick={()=> updateTask(task.id)}	
+							className="btn btn-sm btn-outline-primary py-0 small opacity-75 checkButton ">
+							✓
+						</button>
+					</span>
+				)}	
+				<span>{task.name}</span>						
 			</div>
+
 			<hr className="border border-muted border-1" />
-			<div className="d-flex justify-content-between">
-				<span className="text-muted small">
-					{String(task.createdAt)}
-				</span>				
-				<span>
-					<button className="btn btn-sm btn-outline-secondary py-0 small opacity-50">
-						✎
-					</button>
-					<a className="btn btn-sm btn-outline-danger py-0 small opacity-50">
-                        ×
-					</a>	
-				</span>
-			</div>
+			{task.status ===1 && (			
+				<div className="d-flex justify-content-between">
+					<span className="text-muted small">
+						{String(task.createdAt)}
+					</span>				
+					<span>
+						<button className="btn btn-sm btn-outline-secondary py-0 small ">
+							✎
+						</button>
+						<a className="btn btn-sm btn-outline-danger py-0 small opacity-75">
+							×
+						</a>	
+					</span>
+				</div>
+			)}
 		</div>
     );
 }
