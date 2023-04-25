3cb30941f9780796e0566e90015a126ff41d6667
Blas Cruz <blas.cruz@ucsp.edu.pe>
2023-04-25 16:25:52 -0500

    Primer API flask

---
+++ Backend/backend10/testFlask/colecciondata.py
@@ -0,0 +1,17 @@
+nombre = ['Edu', 'Joel', 'Richard', 'Mike', 'Rick']
+otros = [1, 'Agosto', False, [1,3,4], 10.5]
+
+#Tuplas : solo que no se puede modificar (inmutables)
+notas = (1, 10, 20, 13, 14, 'Eduardo')
+
+# Diccionarios: es ordenado pero por sus llaves No por posiciones
+usuarios = {
+    'nombre':'Eduardo',
+    'apellido':'de Rivero',
+    'edad':30,
+    'soltero': True
+}
+
+#Conjuntos(Set) : es el unica coleccion de datos desordenada y si se puede modificar
+datos = { 10, 80, 'juan','arequipa'}
+print(datos)
--- Backend/backend10/testFlask/initFlask.py
+++ Backend/backend10/testFlask/initFlask.py
@@ -1,19 +1,80 @@
-from flask import Flask 
+from flask import Flask, request
+from flask_cors import CORS
 #  si el archivo  es el archivo principal del proyecto su valor de la
 #  variable  __ name __ sera  '__main__' caso contrario sera None (vacio)
 app = Flask(__name__)
 # patron de diseño de software (singleton)
 
+# ahora configuraremos nuestro CORS ( control de acceso de REcursos Cruzados)
+# origins = '*'
+# methods = '*'
+CORS(app=app, origins=['http://127.0.0.1:5500'], methods=['GET','POST'])
+
+usuarios = [
+    {
+        'nombre':'Eduardo',
+        'apellido':'Juarez'
+    },
+    {
+        'nombre':'Juana',
+        'apellido':'Rodriguez'
+    },
+    {
+        'nombre':'Roberto',
+        'apellido':'Castillo'
+    }
+]
+
 # un decorador se usa con el '@' y sirve para poder modificar cierto
 # metodo de una clase sin la necesidad de modificar el funcionamiento natural 
 # (es una modificacion parcial) luego de utilizar  el decorador se crea una funcion 
 # que sera la nueva funcionalidad de ese metodo 
-@app.route('/')
+@app.route('/') #endpoint
 
 def inicio():
     return 'Hola desde mi servidor de Flask'
 
-
+# ENDPOINT (punto final)  declaracion de la finalizacion de la URL que indicara 
+# que accion se debe realizar 
+@app.route('/mostrar-hora', methods = ['GET','POST'])
+def mostrarHora():
+    # CONTROLLER  (controlador) > es la funcionabilidad que se realizara dentro
+    # de un determinado endpoint
+    # request > me dara toda la informacion que viene desde el cliente
+    print(request.method)
+    # no se  suele retornar strings (cadena de texto)
+    # sino que se utiliza diccionarios
+    if request.method == 'GET':
+        return {
+            'content':'ME hiciste GET'
+        }
+    elif request.method == 'POST':
+        return {
+            'content':'Me hiciste POST'
+        }
+    # aqui no sera accesible ya que los metodos para acceder siempre sera GET o POST
+    return {
+        'content': '22:50:15'
+    }
+    
+@app.route('/usuarios',methods=['GET','POST'])
+def gestionUsuario():
+    if request.method == 'GET':
+        #devolvemos los usuarios
+        return {
+            'message':'Los usuarios son',
+            'content': usuarios
+        }
+    elif request.method == 'POST':
+        # agregar un nuevo usuario
+        # request.data > mostrara la informacion proveniente del body en formato bytes
+        # get_json() > convierte el body entrante en un diccionario  desde un JSON
+        print(request.get_json())
+        data = request.get_json()
+        usuarios.append(data)
+        return {
+            'message':'Usuario agregado exitosamente'
+        }
 # debug > cada vez que modificamos algun archivo del proyecto y guardamos, se
 # reiniciara el servidor 
 app.run(debug=True)
